72eeff6765a446540f508626d730d9e8d0fdb314
Eliot Gannon <egannon@liquid-computing.co.uk>
2019-04-04 17:04:10 +0100

    Update main.js
    wrong file

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-$(function() {
 
-    $('#nav ul li a').click(function(){
-        $('html, body').animate({
-            scrollTop: $("#"+$(this).data('link')).offset().top
-        }, 1000);
-    });
-
-});
